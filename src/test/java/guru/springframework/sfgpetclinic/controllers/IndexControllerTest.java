bad6112a3cea84f39556ceb6315edb4ef2ce16ad
Miłosz Depczyński <depczak@gmail.com>
2019-11-11 09:58:57 +0100

    Timeout assertions

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,5 +32,17 @@ class IndexControllerTest {
                 () -> controller.oopsHandler(false),
                 "RuntimeException should be thrown"
         );
+    }
+
+    @Test
+    @Disabled("Blocks main JUnit thread for 5 seconds.")
+    void testTimeoutAssertion() {
+        assertTimeout(Duration.ofMillis(100), () -> Thread.sleep(5000));
+    }
+
+    @Test
+    void testTimeoutPreemptivelyAssertion() {
+        // kills timeouted thread
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> Thread.sleep(5000));
     }
 }
